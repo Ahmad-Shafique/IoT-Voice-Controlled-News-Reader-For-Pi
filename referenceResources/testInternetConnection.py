a05f655fd17a92d741eaea8c9ba2da08e3fb324a
Ahmad Muhammad Shafique <khan.ahmed20@gmail.com>
2017-04-27 18:38:36 +0600

    Added internet checking code to reference

---
+++ referenceResources/testInternetConnection.py
@@ -0,0 +1,13 @@
+import urllib2
+
+def internet_on():
+    try:
+        urllib2.urlopen('http://216.58.192.142', timeout=1)
+        return True
+    except urllib2.URLError as err: 
+        return False
+        
+if internet_on() :
+	print 'Connected to internet'
+else:
+	print 'Not connected to internet'
